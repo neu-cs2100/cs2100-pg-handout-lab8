47279a5ace67214f579fba0e60f0f9084376e6b8
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2025-08-14 17:43:16 -0400

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,5 +1,13 @@
 from abc import ABC, abstractmethod
 
+"""
+Please implement the Clothing abstract base class according to the comments describing its functionality.
+It will have abstract aspects which are implemented by concrete subclasses.
+
+Then, create two classes which implement the Clothing interface.
+Examples include Shirt, Pants, and Scarf.
+"""
+
 class Clothing(ABC):
     # Interface for Clothing. All of its methods should be abstract.
     @property
@@ -15,6 +23,3 @@ class Clothing(ABC):
         pass
 
 
-
-# Create two classes which implement the Clothing interface.
-# Examples include Shirt, Pants, and Scarf.
